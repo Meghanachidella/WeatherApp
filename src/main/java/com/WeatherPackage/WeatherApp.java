8d571a9d8349c13cdd5cc79fdebe233b7fae7a9d
Meghanachidella <segushanmukasrinivas@gmail.com>
2023-08-03 13:29:30 +0530

    second commit

--- src/main/java/com/WeatherPackage/WeatherApp.java
+++ src/main/java/com/WeatherPackage/WeatherApp.java
@@ -1,10 +1,16 @@
 package com.WeatherPackage;
-import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class WeatherApp {
@@ -27,20 +33,20 @@ public class WeatherApp {
             switch (choice) {
                 case 1:
                     System.out.print("Enter the date (YYYY-MM-DD HH:MM:SS): ");
-                    String date = scanner.nextLine();
-                    double temperature = apiHandler.getTemperature(date);
+                    String dateStr = scanner.nextLine();
+                    double temperature = apiHandler.getTemperature(dateStr);
                     System.out.println("Temperature: " + temperature + " K");
                     break;
                 case 2:
                     System.out.print("Enter the date (YYYY-MM-DD HH:MM:SS): ");
-                    date = scanner.nextLine();
-                    double windSpeed = apiHandler.getWindSpeed(date);
+                    dateStr = scanner.nextLine();
+                    double windSpeed = apiHandler.getWindSpeed(dateStr);
                     System.out.println("Wind Speed: " + windSpeed + " m/s");
                     break;
                 case 3:
                     System.out.print("Enter the date (YYYY-MM-DD HH:MM:SS): ");
-                    date = scanner.nextLine();
-                    double pressure = apiHandler.getPressure(date);
+                    dateStr = scanner.nextLine();
+                    double pressure = apiHandler.getPressure(dateStr);
                     System.out.println("Pressure: " + pressure + " hPa");
                     break;
                 case 0:
@@ -57,64 +63,45 @@ public class WeatherApp {
 
 class WeatherApiHandler {
     private static final String API_URL = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22";
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-    public double getTemperature(String date) {
-        return fetchData(date, "temp");
+    public double getTemperature(String dateStr) {
+        return fetchData(dateStr, "temp");
     }
 
-    public double getWindSpeed(String date) {
-        return fetchData(date, "wind.speed");
+    public double getWindSpeed(String dateStr) {
+        return fetchData(dateStr, "speed");
     }
 
-    public double getPressure(String date) {
-        return fetchData(date, "main.pressure");
+    public double getPressure(String dateStr) {
+        return fetchData(dateStr, "pressure");
     }
 
-  private double fetchData(String date, String field) {
-    try {
-        URL url = new URL(API_URL);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
+    private double fetchData(String dateStr, String field) {
+        try {
+            URL url = new URL(API_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
 
-        Scanner scanner = new Scanner(connection.getInputStream());
-        StringBuilder response = new StringBuilder();
-        while (scanner.hasNextLine()) {
-            response.append(scanner.nextLine());
-        }
-        scanner.close();
+            JsonParser parser = new JsonParser();
+            JsonObject jsonResponse = parser.parse(new InputStreamReader(connection.getInputStream())).getAsJsonObject();
 
-        // Parse the JSON response to get the desired data using Gson
-        Gson gson = new Gson();
-        JsonObject jsonResponse = gson.fromJson(response.toString(), JsonObject.class);
+            JsonArray list = jsonResponse.getAsJsonArray("list");
+            for (int i = 0; i < list.size(); i++) {
+                JsonObject data = list.get(i).getAsJsonObject();
+                String dtTxt = data.get("dt_txt").getAsString();
 
-        // Extract the relevant data based on the 'field' parameter
-        double value = -1.0;
-        if (jsonResponse.has("list")) {
-            JsonElement listElement = jsonResponse.get("list");
-            if (listElement.isJsonArray()) {
-                for (JsonElement element : listElement.getAsJsonArray()) {
-                    if (element.isJsonObject()) {
-                        JsonObject dataObject = element.getAsJsonObject();
-                        if (dataObject.has("dt_txt") && dataObject.get("dt_txt").getAsString().startsWith(date)) {
-                            if (field.equals("temp")) {
-                                value = dataObject.getAsJsonObject("main").get("temp").getAsDouble();
-                            } else if (field.equals("wind.speed")) {
-                                value = dataObject.getAsJsonObject("wind").get("speed").getAsDouble();
-                            } else if (field.equals("main.pressure")) {
-                                value = dataObject.getAsJsonObject("main").get("pressure").getAsDouble();
-                            }
-                            break;
-                        }
-                    }
+                if (dtTxt.startsWith(dateStr)) {
+                    JsonObject main = data.getAsJsonObject("main");
+                    double value = main.get(field).getAsDouble();
+                    return value;
                 }
             }
-        }
 
-        return value;
-    } catch (IOException e) {
-        System.out.println("Error fetching data from API: " + e.getMessage());
-        return -1.0; // Return a sentinel value to indicate an error
+            return -1.0; // Return a sentinel value if data not found
+        } catch (IOException e) {
+            System.out.println("Error fetching data from API: " + e.getMessage());
+            return -1.0; // Return a sentinel value to indicate an error
+        }
     }
 }
-}
-
